464860a6dc152421be1ba13b15cd33dce80285f9
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-11-09 20:07:55 +0800

    rust tool add keymap

--- nvim/after/plugin/rust-tools.lua
+++ nvim/after/plugin/rust-tools.lua
@@ -158,6 +158,12 @@ local opts = {
     -- standalone file support
     -- setting it to false may improve startup time
     standalone = true,
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
   }, -- rust-analyzer options
 
   -- debugging stuff
